a9d128b4e62a5b9084d10a33d0d0841b6f02a2cb
Zomebie <dmsqlcjstk77@naver.com>
2018-11-02 13:37:49 +0900

    my

--- src/controller/CalcController.java
+++ src/controller/CalcController.java
@@ -24,7 +24,7 @@ public class CalcController extends HttpServlet {
 		
 		switch (operator) {
 		case "+":
-			// TODO: result = Integer.parseInt(oper1) + Integer.parseInt(oper2);
+			result = Integer.parseInt(oper1) + Integer.parseInt(oper2);
 			break;
 		case "-":
 			// TODO: result = Integer.parseInt(oper1) - Integer.parseInt(oper2);
